695de2626abddd0e8cc223878df6b95aa244f500
Ozkan Sezer <sezeroz@gmail.com>
2010-06-21 10:55:21 +0000

    * arch_def.h, net_sys.h, wsaerror.h: New headers, adapted from uhexen2.
    git-svn-id: svn://svn.code.sf.net/p/quakespasm/code/trunk/quakespasm@214 af15c1b1-3010-417e-b628-4374ebc0bcbd

---
+++ Quake/arch_def.h
@@ -0,0 +1,117 @@
+/*
+	arch_def.h
+	platform specific definitions
+
+	Copyright (C) 2007  O.Sezer <sezero@users.sourceforge.net>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to:
+
+		Free Software Foundation, Inc.
+		51 Franklin St, Fifth Floor,
+		Boston, MA  02110-1301  USA
+*/
+
+#ifndef __ARCH_DEFS__
+#define __ARCH_DEFS__
+
+
+#if defined(__APPLE__) && defined(__MACH__)
+#   if !defined(__MACOSX__)
+#	define	__MACOSX__		1
+#   endif
+#elif defined(macintosh) /* MacOS Classic */
+#   if !defined(__MACOS__)
+#	define	__MACOS__		1
+#   endif
+#elif (defined(__sun) || defined(sun)) && \
+		(defined(__svr4__) || defined(__SVR4))
+#   if !defined(__SOLARIS__)
+#	define	__SOLARIS__		1
+#   endif
+#elif defined(__sun) || defined(sun)
+#   if !defined(__SUNOS__)
+#	define	__SUNOS__		1
+#   endif
+#elif defined(__sgi) || defined(sgi) || defined(__sgi__) || defined(_SGI_SOURCE)
+#   if !defined(__IRIX__)
+#	define	__IRIX__		1
+#   endif
+#elif defined(__QNXNTO__)
+#   if !defined(__QNX__)
+#	define	__QNX__			1
+#   endif
+#elif defined(__amigados__) || defined(__amigaos4__) || \
+		defined(__AMIGA) || defined(__amigaos__)
+#   if !defined(__AMIGA__)
+#	define	__AMIGA__		1
+#   endif
+#endif	/* end of custom definitions	*/
+
+
+#if defined(__DJGPP__) || defined(MSDOS) || defined(__MSDOS__) || defined(__DOS__)
+
+#   if !defined(PLATFORM_DOS)
+#	define	PLATFORM_DOS		1
+#   endif
+
+#elif defined(_WIN32) || defined(_WIN64)
+
+#   if !defined(PLATFORM_WINDOWS)
+#	define	PLATFORM_WINDOWS	1
+#   endif
+
+#elif defined(__MACOS__) || defined(__MACOSX__)
+
+#   if !defined(PLATFORM_MAC)
+#	define	PLATFORM_MAC		1
+#   endif
+
+#elif defined(__MORPHOS__) || defined(__AMIGA__)
+
+#   if !defined(PLATFORM_AMIGA)
+#	define	PLATFORM_AMIGA		1
+#   endif
+
+#else	/* here goes the unix platforms */
+
+#if defined(__unix) || defined(__unix__) || defined(unix)	|| \
+    defined(__linux__) || defined(__linux)			|| \
+    defined(__FreeBSD__) || defined(__DragonFly__)		|| \
+    defined(__OpenBSD__) || defined(__NetBSD__)			|| \
+    defined(__hpux) || defined(__hpux__) || defined(_hpux)	|| \
+    defined(__sun) || defined(sun) || defined(__IRIX__)		|| \
+    defined(__QNX__)
+#   if !defined(PLATFORM_UNIX)
+#	define	PLATFORM_UNIX		1
+#   endif
+#endif
+
+#endif	/* end of PLATFORM_ definitions */
+
+
+/* Hack section: Platforms that are mostly fine when
+ * classified under PLATFORM_UNIX :
+ */
+#if defined(__MACOSX__)
+
+#   if !defined(PLATFORM_UNIX)
+#	define	PLATFORM_UNIX		2
+#   endif
+
+#endif	/* end of PLATFORM_UNIX hacks */
+
+
+#endif	/* __ARCH_DEFS__ */
+
---
+++ Quake/net_sys.h
@@ -0,0 +1,162 @@
+/*
+	net_sys.h
+	common network system header
+
+	Copyright (C) 2007-2010  O.Sezer <sezero@users.sourceforge.net>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to:
+
+		Free Software Foundation, Inc.
+		51 Franklin St, Fifth Floor,
+		Boston, MA  02110-1301  USA
+*/
+
+#if defined(_USE_SDLNET)
+
+/* can not use the IP banning mechanism in net_dgrm.c with
+   limited SDL_net functionality. */
+#undef	BAN_TEST
+
+#define	sys_socket_t	int
+#define	INVALID_SOCKET	(-1)
+#define	SOCKET_ERROR	(-1)
+
+#ifndef MAXHOSTNAMELEN
+#define MAXHOSTNAMELEN	256
+#endif
+#ifndef AF_INET
+#define AF_INET		2		/* internet */
+#endif
+#ifndef INADDR_LOOPBACK
+#define INADDR_LOOPBACK	0x7f000001	/* 127.0.0.1 */
+#endif
+
+#ifndef __NET_SYS_H__
+#define __NET_SYS_H__	/* the rest below are not needed. */
+#endif
+
+#endif	/* _USE_SDLNET */
+
+
+#ifndef __NET_SYS_H__
+#define __NET_SYS_H__
+
+#include <sys/types.h>
+#include <errno.h>
+#include <stddef.h>
+#include <limits.h>
+
+/* unix includes and compatibility macros */
+#if defined(PLATFORM_UNIX) || defined(PLATFORM_AMIGA)
+
+#include <sys/param.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#if defined(__sun) || defined(sun)
+#include <sys/filio.h>
+#endif	/* __sunos__ */
+
+#if defined(PLATFORM_AMIGA)
+#include <proto/socket.h>
+#endif
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+typedef int	sys_socket_t;
+#define	INVALID_SOCKET	(-1)
+#define	SOCKET_ERROR	(-1)
+
+#if defined(PLATFORM_AMIGA)
+typedef int	socklen_t;
+#define	SOCKETERRNO	Errno()
+#define	ioctlsocket	IoctlSocket
+#define	closesocket	CloseSocket
+#else
+#define	SOCKETERRNO	errno
+#define	ioctlsocket	ioctl
+#define	closesocket	close
+#endif
+
+#define	socketerror(x)	strerror((x))
+
+#endif	/* end of unix stuff */
+
+
+/* windows includes and compatibility macros */
+#if defined(PLATFORM_WINDOWS)
+
+/* NOTE: winsock[2].h already includes windows.h */
+#if !defined(_USE_WINSOCK2)
+#include <winsock.h>
+#else
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#endif
+
+/* there is no in_addr_t on windows: define it as
+   the type of the S_addr of in_addr structure */
+typedef u_long	in_addr_t;	/* uint32_t */
+
+/* on windows, socklen_t is to be a winsock2 thing */
+#if !defined(IP_MSFILTER_SIZE)
+typedef int	socklen_t;
+#endif	/* socklen_t type */
+
+typedef SOCKET	sys_socket_t;
+
+#define	SOCKETERRNO	WSAGetLastError()
+#define	EWOULDBLOCK	WSAEWOULDBLOCK
+#define	ECONNREFUSED	WSAECONNREFUSED
+/* must #include "wsaerror.h" for this : */
+#define	socketerror(x)	__WSAE_StrError((x))
+
+#endif	/* end of windows stuff */
+
+
+/* dos includes and compatibility macros */
+#if defined(PLATFORM_DOS)
+
+/* our local headers : */
+#include "dos/dos_inet.h"
+#include "dos/dos_sock.h"
+
+#endif	/* end of dos stuff. */
+
+
+/* macros which may still be missing */
+
+#if !defined(INADDR_NONE)
+#define	INADDR_NONE	((in_addr_t) 0xffffffff)
+#endif	/* INADDR_NONE */
+
+#if !defined(INADDR_LOOPBACK)
+#define	INADDR_LOOPBACK	((in_addr_t) 0x7f000001)	/* 127.0.0.1	*/
+#endif	/* INADDR_LOOPBACK */
+
+
+#if !defined(MAXHOSTNAMELEN)
+/* SUSv2 guarantees that `Host names are limited to 255 bytes'.
+   POSIX 1003.1-2001 guarantees that `Host names (not including
+   the terminating NUL) are limited to HOST_NAME_MAX bytes'. */
+#define	MAXHOSTNAMELEN		256
+#endif	/* MAXHOSTNAMELEN */
+
+
+#endif	/* __NET_SYS_H__ */
+
