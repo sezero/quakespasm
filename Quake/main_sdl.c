542da2a2aa7f89faa5eb25272fc4c6bc9dd028b4
Kristian Duske <kristian.duske@gmail.com>
2010-08-14 19:41:09 +0000

    git-svn-id: svn://svn.code.sf.net/p/quakespasm/code/trunk/quakespasm@272 af15c1b1-3010-417e-b628-4374ebc0bcbd

--- Quake/main_sdl.c
+++ Quake/main_sdl.c
@@ -63,7 +63,7 @@ int main(int argc, char *argv[])
 		Sys_Error ("Not enough memory free; check disk space\n");
 
 	Con_Printf("FitzQuake %1.2f (c) John Fitzgibbons\n", FITZQUAKE_VERSION);
-	Con_Printf("SDL port (c) Sleepwalkr, Baker\n");
+	Con_Printf("SDL port (c) SleepwalkR, Baker\n");
 	Con_Printf("QuakeSpasm %1.2f.%d (c) Ozkan Sezer, Stevenaaus\n", FITZQUAKE_VERSION, QUAKESPASM_VER_PATCH);
 
 	Con_Printf("Host_Init\n");
