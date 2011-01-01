9289a26a88ffe448cc6a6efd48a5f4b5f217be1c
Ozkan Sezer <sezeroz@gmail.com>
2011-01-01 12:32:16 +0000

    whitespace/formatting tidy-up in quakedef.h.
    git-svn-id: svn://svn.code.sf.net/p/quakespasm/code/trunk/quakespasm@366 af15c1b1-3010-417e-b628-4374ebc0bcbd

--- Quake/quakedef.h
+++ Quake/quakedef.h
@@ -31,14 +31,14 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #define	QUAKE_GAME			// as opposed to utilities
 
-#define	VERSION				1.09
+#define	VERSION			1.09
 #define	GLQUAKE_VERSION		1.00
 #define	D3DQUAKE_VERSION	0.01
 #define	WINQUAKE_VERSION	0.996
 #define	LINUX_VERSION		1.30
-#define	X11_VERSION			1.10
+#define	X11_VERSION		1.10
 
-#define	FITZQUAKE_VERSION	0.85 //johnfitz
+#define	FITZQUAKE_VERSION	0.85	//johnfitz
 #define	QUAKESPASM_VERSION	0.4
 #define	QUAKESPASM_VER_PATCH	4	// helper to print a string like 0.85.4
 
@@ -53,7 +53,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #define UNUSED(x)	(x = x)	// for pesky compiler / lint warnings
 
-#define	MINIMUM_MEMORY			0x550000
+#define	MINIMUM_MEMORY		0x550000
 #define	MINIMUM_MEMORY_LEVELPAK	(MINIMUM_MEMORY + 0x100000)
 
 #define MAX_NUM_ARGVS	50
@@ -73,19 +73,19 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #define	ON_EPSILON		0.1			// point on plane side epsilon
 
-#define	MAX_MSGLEN			32000	// max length of a reliable message //johnfitz -- was 8000
+#define	MAX_MSGLEN		32000	// max length of a reliable message //johnfitz -- was 8000
 #define	MAX_DATAGRAM		32000	// max length of unreliable message //johnfitz -- was 1024
 
-#define DATAGRAM_MTU		1400	// johnfitz -- actual limit for unreliable messages to nonlocal clients
+#define	DATAGRAM_MTU		1400	// johnfitz -- actual limit for unreliable messages to nonlocal clients
 
 //
 // per-level limits
 //
 
-#define MIN_EDICTS		256			// johnfitz -- lowest allowed value for max_edicts cvar
-#define MAX_EDICTS		32000		// johnfitz -- highest allowed value for max_edicts cvar
+#define	MIN_EDICTS		256			// johnfitz -- lowest allowed value for max_edicts cvar
+#define	MAX_EDICTS		32000		// johnfitz -- highest allowed value for max_edicts cvar
 									// ents past 8192 can't play sounds in the standard protocol
-#define DEF_EDICTS		2048
+#define	DEF_EDICTS		2048
 
 #define	MAX_LIGHTSTYLES	64
 #define	MAX_MODELS		2048		// johnfitz -- was 256
@@ -99,16 +99,16 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // stats are integers communicated to the client by the server
 //
 #define	MAX_CL_STATS		32
-#define	STAT_HEALTH			0
-#define	STAT_FRAGS			1
-#define	STAT_WEAPON			2
-#define	STAT_AMMO			3
-#define	STAT_ARMOR			4
+#define	STAT_HEALTH		0
+#define	STAT_FRAGS		1
+#define	STAT_WEAPON		2
+#define	STAT_AMMO		3
+#define	STAT_ARMOR		4
 #define	STAT_WEAPONFRAME	5
-#define	STAT_SHELLS			6
-#define	STAT_NAILS			7
+#define	STAT_SHELLS		6
+#define	STAT_NAILS		7
 #define	STAT_ROCKETS		8
-#define	STAT_CELLS			9
+#define	STAT_CELLS		9
 #define	STAT_ACTIVEWEAPON	10
 #define	STAT_TOTALSECRETS	11
 #define	STAT_TOTALMONSTERS	12
@@ -117,68 +117,68 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 // stock defines
 
-#define	IT_SHOTGUN				1
-#define	IT_SUPER_SHOTGUN		2
-#define	IT_NAILGUN				4
-#define	IT_SUPER_NAILGUN		8
-#define	IT_GRENADE_LAUNCHER		16
-#define	IT_ROCKET_LAUNCHER		32
-#define	IT_LIGHTNING			64
-#define IT_SUPER_LIGHTNING      128
-#define IT_SHELLS               256
-#define IT_NAILS                512
-#define IT_ROCKETS              1024
-#define IT_CELLS                2048
-#define IT_AXE                  4096
-#define IT_ARMOR1               8192
-#define IT_ARMOR2               16384
-#define IT_ARMOR3               32768
-#define IT_SUPERHEALTH          65536
-#define IT_KEY1                 131072
-#define IT_KEY2                 262144
-#define	IT_INVISIBILITY			524288
-#define	IT_INVULNERABILITY		1048576
-#define	IT_SUIT					2097152
-#define	IT_QUAD					4194304
-#define IT_SIGIL1               (1<<28)
-#define IT_SIGIL2               (1<<29)
-#define IT_SIGIL3               (1<<30)
-#define IT_SIGIL4               (1<<31)
+#define	IT_SHOTGUN		1
+#define	IT_SUPER_SHOTGUN	2
+#define	IT_NAILGUN		4
+#define	IT_SUPER_NAILGUN	8
+#define	IT_GRENADE_LAUNCHER	16
+#define	IT_ROCKET_LAUNCHER	32
+#define	IT_LIGHTNING		64
+#define	IT_SUPER_LIGHTNING	128
+#define	IT_SHELLS		256
+#define	IT_NAILS		512
+#define	IT_ROCKETS		1024
+#define	IT_CELLS		2048
+#define	IT_AXE			4096
+#define	IT_ARMOR1		8192
+#define	IT_ARMOR2		16384
+#define	IT_ARMOR3		32768
+#define	IT_SUPERHEALTH		65536
+#define	IT_KEY1			131072
+#define	IT_KEY2			262144
+#define	IT_INVISIBILITY		524288
+#define	IT_INVULNERABILITY	1048576
+#define	IT_SUIT			2097152
+#define	IT_QUAD			4194304
+#define	IT_SIGIL1		(1<<28)
+#define	IT_SIGIL2		(1<<29)
+#define	IT_SIGIL3		(1<<30)
+#define	IT_SIGIL4		(1<<31)
 
 //===========================================
 //rogue changed and added defines
 
-#define RIT_SHELLS              128
-#define RIT_NAILS               256
-#define RIT_ROCKETS             512
-#define RIT_CELLS               1024
-#define RIT_AXE                 2048
-#define RIT_LAVA_NAILGUN        4096
-#define RIT_LAVA_SUPER_NAILGUN  8192
-#define RIT_MULTI_GRENADE       16384
-#define RIT_MULTI_ROCKET        32768
-#define RIT_PLASMA_GUN          65536
-#define RIT_ARMOR1              8388608
-#define RIT_ARMOR2              16777216
-#define RIT_ARMOR3              33554432
-#define RIT_LAVA_NAILS          67108864
-#define RIT_PLASMA_AMMO         134217728
-#define RIT_MULTI_ROCKETS       268435456
-#define RIT_SHIELD              536870912
-#define RIT_ANTIGRAV            1073741824
-#define RIT_SUPERHEALTH         2147483648
+#define	RIT_SHELLS		128
+#define	RIT_NAILS		256
+#define	RIT_ROCKETS		512
+#define	RIT_CELLS		1024
+#define	RIT_AXE			2048
+#define	RIT_LAVA_NAILGUN	4096
+#define	RIT_LAVA_SUPER_NAILGUN	8192
+#define	RIT_MULTI_GRENADE	16384
+#define	RIT_MULTI_ROCKET	32768
+#define	RIT_PLASMA_GUN		65536
+#define	RIT_ARMOR1		8388608
+#define	RIT_ARMOR2		16777216
+#define	RIT_ARMOR3		33554432
+#define	RIT_LAVA_NAILS		67108864
+#define	RIT_PLASMA_AMMO		134217728
+#define	RIT_MULTI_ROCKETS	268435456
+#define	RIT_SHIELD		536870912
+#define	RIT_ANTIGRAV		1073741824
+#define	RIT_SUPERHEALTH		2147483648
 
 //MED 01/04/97 added hipnotic defines
 //===========================================
 //hipnotic added defines
-#define HIT_PROXIMITY_GUN_BIT 16
-#define HIT_MJOLNIR_BIT       7
-#define HIT_LASER_CANNON_BIT  23
-#define HIT_PROXIMITY_GUN   (1<<HIT_PROXIMITY_GUN_BIT)
-#define HIT_MJOLNIR         (1<<HIT_MJOLNIR_BIT)
-#define HIT_LASER_CANNON    (1<<HIT_LASER_CANNON_BIT)
-#define HIT_WETSUIT         (1<<(23+2))
-#define HIT_EMPATHY_SHIELDS (1<<(23+3))
+#define	HIT_PROXIMITY_GUN_BIT	16
+#define	HIT_MJOLNIR_BIT		7
+#define	HIT_LASER_CANNON_BIT	23
+#define	HIT_PROXIMITY_GUN	(1<<HIT_PROXIMITY_GUN_BIT)
+#define	HIT_MJOLNIR		(1<<HIT_MJOLNIR_BIT)
+#define	HIT_LASER_CANNON	(1<<HIT_LASER_CANNON_BIT)
+#define	HIT_WETSUIT		(1<<(23+2))
+#define	HIT_EMPATHY_SHIELDS	(1<<(23+3))
 
 //===========================================
 
@@ -214,14 +214,14 @@ typedef struct
 
 typedef struct
 {
-	vec3_t			origin;
-	vec3_t			angles;
-	unsigned short 	modelindex; //johnfitz -- was int
-	unsigned short 	frame; //johnfitz -- was int
-	unsigned char 	colormap; //johnfitz -- was int
-	unsigned char 	skin; //johnfitz -- was int
-	unsigned char	alpha; //johnfitz -- added
-	int			effects;
+	vec3_t		origin;
+	vec3_t		angles;
+	unsigned short 	modelindex;	//johnfitz -- was int
+	unsigned short 	frame;		//johnfitz -- was int
+	unsigned char 	colormap;	//johnfitz -- was int
+	unsigned char 	skin;		//johnfitz -- was int
+	unsigned char	alpha;		//johnfitz -- added
+	int		effects;
 } entity_state_t;
 
 
@@ -240,8 +240,8 @@ typedef struct
 #include "server.h"
 #include "gl_model.h"
 
-#include "image.h" //johnfitz
-#include "gl_texmgr.h" //johnfitz
+#include "image.h"	//johnfitz
+#include "gl_texmgr.h"	//johnfitz
 
 #include "input.h"
 #include "world.h"
@@ -272,12 +272,12 @@ extern	cvar_t		sys_nostdout;
 extern	cvar_t		developer;
 extern	cvar_t		max_edicts; //johnfitz
 
-extern	qboolean	host_initialized;		// true if into command execution
+extern	qboolean	host_initialized;	// true if into command execution
 extern	double		host_frametime;
 extern	byte		*host_colormap;
-extern	int			host_framecount;	// incremented every frame, never reset
-extern	double		realtime;			// not bounded in any way, changed at
-										// start of every frame, never reset
+extern	int		host_framecount;	// incremented every frame, never reset
+extern	double		realtime;		// not bounded in any way, changed at
+							// start of every frame, never reset
 
 void Host_ClearMemory (void);
 void Host_ServerFrame (void);
@@ -295,13 +295,13 @@ void Host_WriteConfiguration (void);
 void ExtraMaps_Init (void);
 void Modlist_Init (void);
 
-extern int			current_skill;		// skill level for currently loaded level (in case
-										//  the user changes the cvar while the level is
-										//  running, this reflects the level actually in use)
+extern int		current_skill;		// skill level for currently loaded level (in case
+						//  the user changes the cvar while the level is
+						//  running, this reflects the level actually in use)
 
 extern qboolean		isDedicated;
 
-extern int			minimum_memory;
+extern int		minimum_memory;
 
 //
 // chase
@@ -310,8 +310,8 @@ extern	cvar_t	chase_active;
 
 void Chase_Init (void);
 void TraceLine (vec3_t start, vec3_t end, vec3_t impact);
-void Chase_UpdateForClient (void); //johnfitz
-void Chase_UpdateForDrawing (void); //johnfitz
+void Chase_UpdateForClient (void);	//johnfitz
+void Chase_UpdateForDrawing (void);	//johnfitz
 
 #endif	/* __QUAKEDEFS_H */
 
