9bb413d4aaf29f2decb938c1e6aa65b860af6219
Ozkan Sezer <sezeroz@gmail.com>
2011-01-05 21:29:00 +0000

    snd_mp3.c: fixed windows build.
    git-svn-id: svn://svn.code.sf.net/p/quakespasm/code/trunk/quakespasm@377 af15c1b1-3010-417e-b628-4374ebc0bcbd

--- Quake/snd_mp3.c
+++ Quake/snd_mp3.c
@@ -278,7 +278,7 @@ static int mp3_decode(snd_stream_t *stream, byte *buf, int len)
 	do
 	{
 		x = (p->Synth.pcm.length - p->cursamp) * stream->info.channels;
-		donow = q_min(len, x);
+		donow = min(len, x);
 		i = 0;
 		while (i < donow)
 		{
