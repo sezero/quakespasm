00545bbbed8d4a797c93779fb03c5f253b144441
Ozkan Sezer <sezeroz@gmail.com>
2011-01-10 20:17:15 +0000

    bgm*: updated copyright years. bgmnull.c: removed unused file.
    git-svn-id: svn://svn.code.sf.net/p/quakespasm/code/trunk/quakespasm@387 af15c1b1-3010-417e-b628-4374ebc0bcbd

--- Quake/bgmusic.c
+++ Quake/bgmusic.c
@@ -3,7 +3,7 @@
  * Handles streaming music as raw sound samples and runs the midi driver
  *
  * Copyright (C) 1999-2005 Id Software, Inc.
- * Copyright (C) 2011 O.Sezer <sezero@users.sourceforge.net>
+ * Copyright (C) 2010-2011 O.Sezer <sezero@users.sourceforge.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- Quake/bgmusic.h
+++ Quake/bgmusic.h
@@ -3,7 +3,7 @@
  * Handles streaming music as raw sound samples and runs the midi driver
  *
  * Copyright (C) 1999-2005 Id Software, Inc.
- * Copyright (C) 2011 O.Sezer <sezero@users.sourceforge.net>
+ * Copyright (C) 2010-2011 O.Sezer <sezero@users.sourceforge.net>
  *
  * $Id: bgmusic.h 3818 2010-12-19 09:04:17Z sezero $
  *
