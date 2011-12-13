db8569aa86f43407da0235d648c68100099657f8
Ozkan Sezer <sezeroz@gmail.com>
2011-12-13 19:00:25 +0000

    CL_ParseUpdate: removed unused bitcounts[] array
    git-svn-id: svn://svn.code.sf.net/p/quakespasm/code/trunk/quakespasm@516 af15c1b1-3010-417e-b628-4374ebc0bcbd

--- Quake/cl_parse.c
+++ Quake/cl_parse.c
@@ -403,17 +403,15 @@ If an entities model or origin changes from frame to frame, it must be
 relinked.  Other attributes can change without relinking.
 ==================
 */
-int	bitcounts[16];
-
 void CL_ParseUpdate (int bits)
 {
-	int			i;
+	int		i;
 	model_t		*model;
-	int			modnum;
+	int		modnum;
 	qboolean	forcelink;
 	entity_t	*ent;
-	int			num;
-	int			skin;
+	int		num;
+	int		skin;
 
 	if (cls.signon == SIGNONS - 1)
 	{	// first update is the final signon stage
@@ -443,10 +441,6 @@ void CL_ParseUpdate (int bits)
 		num = MSG_ReadByte ();
 
 	ent = CL_EntityNum (num);
-
-	for (i=0 ; i<16 ; i++)
-		if (bits&(1<<i))
-			bitcounts[i]++;
 
 	if (ent->msgtime != cl.mtime[1])
 		forcelink = true;	// no previous frame to lerp from
