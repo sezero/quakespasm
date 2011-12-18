34025248882b5dfc815d02f34515cd02302c2cd9
Ozkan Sezer <sezeroz@gmail.com>
2011-12-18 09:51:28 +0000

    net_loop.h: Tiny whitespace tidy up.
    git-svn-id: svn://svn.code.sf.net/p/quakespasm/code/trunk/quakespasm@526 af15c1b1-3010-417e-b628-4374ebc0bcbd

--- Quake/net_loop.h
+++ Quake/net_loop.h
@@ -23,15 +23,14 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define __NET_LOOP_H
 
 // net_loop.h
-
-int			Loop_Init (void);
+int		Loop_Init (void);
 void		Loop_Listen (qboolean state);
 void		Loop_SearchForHosts (qboolean xmit);
 qsocket_t	*Loop_Connect (const char *host);
 qsocket_t	*Loop_CheckNewConnections (void);
-int			Loop_GetMessage (qsocket_t *sock);
-int			Loop_SendMessage (qsocket_t *sock, sizebuf_t *data);
-int			Loop_SendUnreliableMessage (qsocket_t *sock, sizebuf_t *data);
+int		Loop_GetMessage (qsocket_t *sock);
+int		Loop_SendMessage (qsocket_t *sock, sizebuf_t *data);
+int		Loop_SendUnreliableMessage (qsocket_t *sock, sizebuf_t *data);
 qboolean	Loop_CanSendMessage (qsocket_t *sock);
 qboolean	Loop_CanSendUnreliableMessage (qsocket_t *sock);
 void		Loop_Close (qsocket_t *sock);
