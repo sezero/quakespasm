5e6212ad026ba3aa94e9757f279abbb7c2e464b8
Ozkan Sezer <sezeroz@gmail.com>
2011-12-27 11:03:39 +0000

    added strlcat.c and strlcpy.c: will use them later.
    git-svn-id: svn://svn.code.sf.net/p/quakespasm/code/trunk/quakespasm@558 af15c1b1-3010-417e-b628-4374ebc0bcbd

--- Quake/common.h
+++ Quake/common.h
@@ -138,6 +138,9 @@ int Q_strncasecmp (const char *s1, const char *s2, int n);
 int	Q_atoi (const char *str);
 float Q_atof (const char *str);
 
+
+#include "strl_fn.h"
+
 /* snprintf, vsnprintf : always use our versions. */
 /* platform dependant (v)snprintf function names: */
 #if defined(_WIN32)
