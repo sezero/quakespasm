b714fbb601fdaf5e30560cb396c300aa9435d4cd
Sander van Dijk <a.h.vandijk@gmail.com>
2012-01-17 08:34:54 +0000

    gl_screen.c:SCR_ModalMessage(): sleep a little while busy-waiting.
    git-svn-id: svn://svn.code.sf.net/p/quakespasm/code/trunk/quakespasm@610 af15c1b1-3010-417e-b628-4374ebc0bcbd

--- Quake/gl_screen.c
+++ Quake/gl_screen.c
@@ -884,6 +884,7 @@ int SCR_ModalMessage (const char *text, float timeout) //johnfitz -- timeout
 		key_count = -1;		// wait for a key down and up
 		Sys_SendKeyEvents ();
 		if (timeout) time2 = Sys_DoubleTime (); //johnfitz -- zero timeout means wait forever.
+		Sys_Sleep(16);
 	} while (key_lastpress != 'y' &&
 			 key_lastpress != 'n' &&
 			 key_lastpress != K_ESCAPE &&
