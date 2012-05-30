5c87f99ad599f1af6d3e142601284117bf1a5425
Ozkan Sezer <sezeroz@gmail.com>
2012-05-30 09:00:12 +0000

    removed the SNDDMA_GetSamples() call from snd_dma.c:GetSoundtime() for solaris
    git-svn-id: svn://svn.code.sf.net/p/quakespasm/code/trunk/quakespasm@677 af15c1b1-3010-417e-b628-4374ebc0bcbd

--- Quake/snd_dma.c
+++ Quake/snd_dma.c
@@ -862,9 +862,6 @@ static void GetSoundtime (void)
 
 // it is possible to miscount buffers if it has wrapped twice between
 // calls to S_Update.  Oh well.
-#ifdef __sun__
-	soundtime = SNDDMA_GetSamples();
-#else
 	samplepos = SNDDMA_GetDMAPos();
 
 	if (samplepos < oldsamplepos)
@@ -881,7 +878,6 @@ static void GetSoundtime (void)
 	oldsamplepos = samplepos;
 
 	soundtime = buffers*fullsamples + samplepos/shm->channels;
-#endif
 }
 
 void S_ExtraUpdate (void)
