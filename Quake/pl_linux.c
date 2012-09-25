109378cf692df3e84a973437f5be5d8f9f21546c
Sander van Dijk <a.h.vandijk@gmail.com>
2012-09-25 03:07:33 +0000

    Simplify the linux icon transparency setting.
    git-svn-id: svn://svn.code.sf.net/p/quakespasm/code/trunk/quakespasm@730 af15c1b1-3010-417e-b628-4374ebc0bcbd

--- Quake/pl_linux.c
+++ Quake/pl_linux.c
@@ -32,55 +32,11 @@ static const Uint8 bmp_bytes[] =
 #include "qs_bmp.h"
 };
 
-static Uint8 *PL_CreateIconMask(SDL_Surface *icon)
-{
-	int	x, y;
-	int	i;
-	Uint8	*topleft_pixel, *current_pixel;
-	Uint8	*mask;
-
-	/* Note that this sets all pixels to invisible in the mask. */
-	mask = (Uint8 *) calloc(((icon->w + 7) / 8) * icon->h, sizeof(Uint8));
-	if (!mask)
-		return NULL;
-
-	if (SDL_MUSTLOCK(icon))
-		SDL_LockSurface(icon);
-
-	/* The top-left pixel and identical pixels will remain invisible. */
-	topleft_pixel = (Uint8 *)icon->pixels;
-	for (x = 0; x < icon->w; x++)
-	{
-		for (y = 0; y < icon->h; y++)
-		{
-			current_pixel = (Uint8 *)icon->pixels +
-					y * icon->pitch +
-					x * icon->format->BytesPerPixel;
-			for (i = 0; i < icon->format->BytesPerPixel; i++)
-			{
-				/* If the current pixel is not identical to
-				 * the top-left pixel, set it to visible in
-				 * the mask. */
-				if (current_pixel[i] != topleft_pixel[i])
-				{
-					mask[y * ((icon->w + 7) / 8) + x / 8] |= 128 >> x % 8;
-					break;
-				}
-			}
-		}
-	}
-
-	if (SDL_MUSTLOCK(icon))
-		SDL_UnlockSurface(icon);
-
-	return mask;
-}
-
 void PL_SetWindowIcon (void)
 {
 	SDL_RWops	*rwop;
 	SDL_Surface	*icon;
-	Uint8		*mask;
+	Uint32		colorkey;
 
 	/* SDL_RWFromConstMem() requires SDL >= 1.2.7 */
 	rwop = SDL_RWFromConstMem(bmp_bytes, sizeof(bmp_bytes));
@@ -89,12 +45,9 @@ void PL_SetWindowIcon (void)
 	icon = SDL_LoadBMP_RW(rwop, 1);
 	if (icon == NULL)
 		return;
-	mask = PL_CreateIconMask(icon);
-	if (mask != NULL)
-	{
-		SDL_WM_SetIcon(icon, mask);
-		free(mask);
-	}
+	colorkey = SDL_MapRGB(icon->format, 255, 0, 255);
+	SDL_SetColorKey(icon, SDL_SRCCOLORKEY, colorkey);
+	SDL_WM_SetIcon(icon, NULL);
 	SDL_FreeSurface(icon);
 }
 
