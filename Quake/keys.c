a3f4bdbbabea6cfd44bac95e140e60a6c123bc44
Ozkan Sezer <sezeroz@gmail.com>
2012-09-28 22:24:27 +0000

    include arch_def.h in keys.c
    git-svn-id: svn://svn.code.sf.net/p/quakespasm/code/trunk/quakespasm@747 af15c1b1-3010-417e-b628-4374ebc0bcbd

--- Quake/keys.c
+++ Quake/keys.c
@@ -21,6 +21,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include "quakedef.h"
+#include "arch_def.h"
 
 /* key up events are sent even if in console mode */
 
