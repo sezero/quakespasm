00e44d6227a075d3fcd1cd568e810aedafc703cf
Ozkan Sezer <sezeroz@gmail.com>
2013-03-15 12:47:08 +0000

    r_part.c (R_EntityParticles): fix undefined behavior in the loop init'ing avelocities, thanks to a -Waggressive-loop-optimizations warning from gcc-4.8.
    git-svn-id: svn://svn.code.sf.net/p/quakespasm/code/trunk/quakespasm@842 af15c1b1-3010-417e-b628-4374ebc0bcbd

--- Quake/r_part.c
+++ Quake/r_part.c
@@ -202,8 +202,12 @@ void R_EntityParticles (entity_t *ent)
 
 	if (!avelocities[0][0])
 	{
-		for (i = 0; i < NUMVERTEXNORMALS*3; i++)
-			avelocities[0][i] = (rand() & 255) * 0.01;
+		for (i = 0; i < NUMVERTEXNORMALS; i++)
+		{
+			avelocities[i][0] = (rand() & 255) * 0.01;
+			avelocities[i][1] = (rand() & 255) * 0.01;
+			avelocities[i][2] = (rand() & 255) * 0.01;
+		}
 	}
 
 	for (i = 0; i < NUMVERTEXNORMALS; i++)
