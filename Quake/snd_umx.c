e17a2459b78a14cb08ca1905e0d7592f70af5465
Ozkan Sezer <sezeroz@gmail.com>
2014-01-12 18:27:33 +0000

    fix umx export table read order. doesn't affect our umx playback, but correctness is good.
    git-svn-id: svn://svn.code.sf.net/p/quakespasm/code/trunk/quakespasm@887 af15c1b1-3010-417e-b628-4374ebc0bcbd

--- Quake/snd_umx.c
+++ Quake/snd_umx.c
@@ -240,8 +240,8 @@ static int probe_umx   (fshandle_t *f, const struct upkg_hdr *hdr,
 	FS_fread(buf, 1, 64, f);
 
 	get_fci(&buf[idx], &idx);	/* skip class_index */
-	if (hdr->file_version >= 60) idx += 4; /* skip int32 package_index */
 	get_fci(&buf[idx], &idx);	/* skip super_index */
+	if (hdr->file_version >= 60) idx += 4; /* skip int32 package_index */
 	get_fci(&buf[idx], &idx);	/* skip object_name */
 	idx += 4;			/* skip int32 object_flags */
 
