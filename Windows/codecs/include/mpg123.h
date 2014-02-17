8e37e6d77c4e8f710d1fca0a159a83eba54e9f33
Ozkan Sezer <sezeroz@gmail.com>
2014-02-17 18:00:25 +0000

    oslibs, codecs (win, osx): upgrade libmpg123 to stable 1.18.1 release.
    git-svn-id: svn://svn.code.sf.net/p/quakespasm/code/trunk/quakespasm@895 af15c1b1-3010-417e-b628-4374ebc0bcbd

--- Windows/codecs/include/mpg123.h
+++ Windows/codecs/include/mpg123.h
@@ -1,5 +1,5 @@
 /*
-	libmpg123: MPEG Audio Decoder library (version 1.18.0)
+	libmpg123: MPEG Audio Decoder library (version 1.18.1)
 
 	copyright 1995-2010 by the mpg123 project - free software under the terms of the LGPL 2.1
 	see COPYING and AUTHORS files in distribution or http://mpg123.org
