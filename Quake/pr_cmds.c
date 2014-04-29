1463ffca519260ced5f6ab8049c5fee3b17890b5
Steven <stevenaaus@gmail.com>
2014-04-29 08:28:52 +0000

    Make sv_aim persistant.
    git-svn-id: svn://svn.code.sf.net/p/quakespasm/code/trunk/quakespasm@911 af15c1b1-3010-417e-b628-4374ebc0bcbd

--- Quake/pr_cmds.c
+++ Quake/pr_cmds.c
@@ -1325,7 +1325,7 @@ Pick a vector for the player to shoot along
 vector aim(entity, missilespeed)
 =============
 */
-cvar_t	sv_aim = {"sv_aim", "0.93", CVAR_NONE};
+cvar_t	sv_aim = {"sv_aim", "0.93", CVAR_ARCHIVE};
 static void PF_aim (void)
 {
 	edict_t	*ent, *check, *bestent;
