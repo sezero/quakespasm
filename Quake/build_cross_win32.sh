00846e545df1e082e0d76d2a6dadd98f92e3ff18
Ozkan Sezer <sezeroz@gmail.com>
2015-03-02 22:01:12 +0000

    fix the cross-build scripts after r1180.
    git-svn-id: svn://svn.code.sf.net/p/quakespasm/code/trunk/quakespasm@1181 af15c1b1-3010-417e-b628-4374ebc0bcbd

--- Quake/build_cross_win32.sh
+++ Quake/build_cross_win32.sh
@@ -2,7 +2,6 @@
 
 # Change this script to meet your needs and/or environment.
 
-#TARGET=i386-mingw32msvc
 #TARGET=i686-w64-mingw32
 TARGET=i686-pc-mingw32
 #PREFIX=/opt/cross_win32
@@ -13,7 +12,6 @@ export PATH
 
 MAKE_CMD=make
 
-SDL_CONFIG=/usr/local/sdl_w32/bin/sdl-config
 CC="$TARGET-gcc"
 AS="$TARGET-as"
 RANLIB="$TARGET-ranlib"
@@ -22,4 +20,4 @@ WINDRES="$TARGET-windres"
 STRIP="$TARGET-strip"
 export PATH CC AS AR RANLIB WINDRES STRIP
 
-exec $MAKE_CMD SDL_CONFIG=$SDL_CONFIG CC=$CC AS=$AS RANLIB=$RANLIB AR=$AR WINDRES=$WINDRES STRIP=$STRIP -f Makefile.w32 $*
+exec $MAKE_CMD CC=$CC AS=$AS RANLIB=$RANLIB AR=$AR WINDRES=$WINDRES STRIP=$STRIP -f Makefile.w32 $*
--- Quake/build_cross_win64.sh
+++ Quake/build_cross_win64.sh
@@ -2,7 +2,6 @@
 
 # Change this script to meet your needs and/or environment.
 
-#TARGET=x86_64-pc-mingw32
 TARGET=x86_64-w64-mingw32
 PREFIX=/opt/cross_win64
 
@@ -11,7 +10,6 @@ export PATH
 
 MAKE_CMD=make
 
-SDL_CONFIG=/opt/sdl_w64/bin/sdl-config
 CC="$TARGET-gcc"
 AS="$TARGET-as"
 RANLIB="$TARGET-ranlib"
@@ -20,4 +18,4 @@ WINDRES="$TARGET-windres"
 STRIP="$TARGET-strip"
 export PATH CC AS AR RANLIB WINDRES STRIP
 
-exec $MAKE_CMD SDL_CONFIG=$SDL_CONFIG CC=$CC AS=$AS RANLIB=$RANLIB AR=$AR WINDRES=$WINDRES STRIP=$STRIP -f Makefile.w64 $*
+exec $MAKE_CMD CC=$CC AS=$AS RANLIB=$RANLIB AR=$AR WINDRES=$WINDRES STRIP=$STRIP -f Makefile.w64 $*
