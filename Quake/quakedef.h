4c7ebfd20ba09f2a2074351146b82f55026fa675
Ozkan Sezer <sezeroz@gmail.com>
2015-06-15 08:21:14 +0000

    bumped version to 0.91.0 for future development
    git-svn-id: svn://svn.code.sf.net/p/quakespasm/code/trunk/quakespasm@1230 af15c1b1-3010-417e-b628-4374ebc0bcbd

--- Quake/quakedef.h
+++ Quake/quakedef.h
@@ -36,8 +36,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define	X11_VERSION		1.10
 
 #define	FITZQUAKE_VERSION	0.85	//johnfitz
-#define	QUAKESPASM_VERSION	0.90
-#define	QUAKESPASM_VER_PATCH	1	// helper to print a string like 0.90.0
+#define	QUAKESPASM_VERSION	0.91
+#define	QUAKESPASM_VER_PATCH	0	// helper to print a string like 0.91.0
 
 //define	PARANOID			// speed sapping error checking
 
