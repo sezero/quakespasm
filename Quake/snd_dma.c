35631f4b02ffcbc81c4cfc8948b14f5adc8bf6ae
Eric Wasylishen <ewasylishen@gmail.com>
2015-12-22 06:22:29 +0000

    snd_dma.c: raise MAX_SFX to 1024, for the upcoming arcadim mod. Uses only 36k extra of hunk memory, less for 32-bit builds, so shouldn't have any negative effects.
    git-svn-id: svn://svn.code.sf.net/p/quakespasm/code/trunk/quakespasm@1266 af15c1b1-3010-417e-b628-4374ebc0bcbd

--- Quake/snd_dma.c
+++ Quake/snd_dma.c
@@ -62,7 +62,7 @@ int		s_rawend;
 portable_samplepair_t	s_rawsamples[MAX_RAW_SAMPLES];
 
 
-#define	MAX_SFX		512
+#define	MAX_SFX		1024
 static sfx_t	*known_sfx = NULL;	// hunk allocated [MAX_SFX]
 static int	num_sfx;
 
