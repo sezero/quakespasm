f095cd1abf722f94497c631d23a0f57a206a86cd
Ozkan Sezer <sezeroz@gmail.com>
2016-10-01 11:00:35 +0000

    filenames.h: update copyright years from mainstream.
    git-svn-id: svn://svn.code.sf.net/p/quakespasm/code/trunk/quakespasm@1344 af15c1b1-3010-417e-b628-4374ebc0bcbd

--- Quake/filenames.h
+++ Quake/filenames.h
@@ -5,10 +5,9 @@
  * use forward- and back-slash in path names interchangeably, and
  * some of them have case-insensitive file names.
  *
- * Copyright 2000, 2001, 2007 Free Software Foundation, Inc.
- *
- * This is based on filenames.h from BFD, the Binary File Descriptor
- * library, changed further for our needs.
+ * This was based on filenames.h from BFD, the Binary File Descriptor
+ * library, Copyright (C) 2000-2016 Free Software Foundation, Inc.,
+ * and changed by O. Sezer <sezero@users.sourceforge.net> for our needs.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
