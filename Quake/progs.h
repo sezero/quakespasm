3cefb58d5d6049468751096f04f9a0d73a32930e
Ozkan Sezer <sezeroz@gmail.com>
2017-09-16 19:50:12 +0000

    move the watcom 'aborts' pragma after function declarations.
    git-svn-id: svn://svn.code.sf.net/p/quakespasm/code/trunk/quakespasm@1503 af15c1b1-3010-417e-b628-4374ebc0bcbd

--- Quake/progs.h
+++ Quake/progs.h
@@ -130,10 +130,10 @@ extern	int		pr_xstatement;
 
 extern	unsigned short	pr_crc;
 
+FUNC_NORETURN void PR_RunError (const char *error, ...) FUNC_PRINTF(1,2);
 #ifdef __WATCOMC__
 #pragma aux PR_RunError aborts;
 #endif
-FUNC_NORETURN void PR_RunError (const char *error, ...) FUNC_PRINTF(1,2);
 
 void ED_PrintEdicts (void);
 void ED_PrintNum (int ent);
