0b27fd71d9003d6e4fbec0ef55ec9b71ee57dfa0
IvoryDuke <iv0ryduk3@gmail.com>
2020-08-04 17:21:13 +0200

    Don't show unbound keys warning messages when console is opened

--- Quake/keys.c
+++ Quake/keys.c
@@ -962,12 +962,12 @@ void Key_Event (int key, qboolean down)
 			if (key_dest == key_game && !con_forcedup)
 				return; // ignore autorepeats in game mode
 		}
-		else if (key >= 200 && !keybindings[key])
+		else if (key >= 200 && !keybindings[key] && key_dest != key_console)
 			Con_Printf ("%s is unbound, hit F4 to set.\n", Key_KeynumToString(key));
 	}
 	else if (!keydown[key])
 		return; // ignore stray key up events
-
+	
 	keydown[key] = down;
 
 	if (key_inputgrab.active)
