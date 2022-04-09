60191aefa060eaff23bcf76056424e240ec36244
Ozkan Sezer <sezeroz@gmail.com>
2022-04-09 14:33:02 +0300

    upgrade to SDL2-2.0.22-rc1

--- MacOSX/SDL2.framework/Versions/A/Headers/SDL_revision.h
+++ MacOSX/SDL2.framework/Versions/A/Headers/SDL_revision.h
@@ -1,2 +1,2 @@
-#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@c2904c8ce70cb1c295740ab5d65d36d226ead11f"
+#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@505d6a4a052592b2676f87456c1f564daa8d2c50"
 #define SDL_REVISION_NUMBER 0
