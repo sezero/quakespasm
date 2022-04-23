ae4bf378c03a76e227af7aaad1bf85dc14e0f491
Ozkan Sezer <sezeroz@gmail.com>
2022-04-23 11:03:20 +0300

    fix getting stuck with loading plaque upon loading bad saves using menu
    also change bad save versions to Host_Error and change a Sys_Error into
    Host_Error.

--- Quake/host_cmd.c
+++ Quake/host_cmd.c
@@ -1177,7 +1177,7 @@ static void Host_Loadgame_f (void)
 	{
 		free (start);
 		start = NULL;
-		Con_Printf ("Savegame is version %i, not %i\n", version, SAVEGAME_VERSION);
+		Host_Error ("Savegame is version %i, not %i", version, SAVEGAME_VERSION);
 		return;
 	}
 	data = COM_ParseStringNewline (data);
@@ -1200,6 +1200,7 @@ static void Host_Loadgame_f (void)
 	{
 		free (start);
 		start = NULL;
+		SCR_EndLoadingPlaque ();
 		Con_Printf ("Couldn't load map\n");
 		return;
 	}
@@ -1207,7 +1208,6 @@ static void Host_Loadgame_f (void)
 	sv.loadgame = true;
 
 // load the light styles
-
 	for (i = 0; i < MAX_LIGHTSTYLES; i++)
 	{
 		data = COM_ParseStringNewline (data);
@@ -1223,7 +1223,7 @@ static void Host_Loadgame_f (void)
 			break;		// end of file
 		if (strcmp(com_token,"{"))
 		{
-			Sys_Error ("First token isn't a brace");
+			Host_Error ("First token isn't a brace");
 		}
 
 		if (entnum == -1)
