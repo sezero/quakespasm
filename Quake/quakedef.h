0dd1c12dd9f405cd8d9ecf96f86425bbe268876d
Ozkan Sezer <sezeroz@gmail.com>
2022-05-24 20:55:04 +0300

    bump version to 0.94.5, update documents.

--- Quake/quakedef.h
+++ Quake/quakedef.h
@@ -37,7 +37,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #define	FITZQUAKE_VERSION	0.85	//johnfitz
 #define	QUAKESPASM_VERSION	0.94
-#define	QUAKESPASM_VER_PATCH	4	// helper to print a string like 0.94.4
+#define	QUAKESPASM_VER_PATCH	5	// helper to print a string like 0.94.4
 #ifndef	QUAKESPASM_VER_SUFFIX
 #define	QUAKESPASM_VER_SUFFIX		// optional version suffix string literal like "-beta1"
 #endif
