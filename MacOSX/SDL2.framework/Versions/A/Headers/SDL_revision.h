56b7c97549f44fb190656139b60ed45f7aba0c6a
Ozkan Sezer <sezeroz@gmail.com>
2023-09-03 20:15:32 +0300

    updated builds of SDL2 (macOS), opusfile, and xmp.

--- MacOSX/SDL2.framework/Versions/A/Headers/SDL_revision.h
+++ MacOSX/SDL2.framework/Versions/A/Headers/SDL_revision.h
@@ -1,2 +1,2 @@
-#define SDL_REVISION "https://github.com/sezero/SDL.git@07bb804955f66a954dbe7a71db29d76847de69f2"
+#define SDL_REVISION "https://github.com/sezero/SDL.git@c947e0e31b673909ec91509f38b34028f608fb6f"
 #define SDL_REVISION_NUMBER 0
