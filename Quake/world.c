0604ce8beb8850595f1281dcddbea8ec5d28feb1
Andrei Drexler <makro@rq3.com>
2024-03-22 22:42:28 +0100

    Improve handling of huge entities in SV_FindTouchedLeafs
    This increases performance significantly on replicon
    https://quaketastic.com/files/single_player/maps/replicon.zip
    https://www.celephais.net/board/view_thread.php?id=62305&start=
    
    (cherry picked from commit 2a96fde31a5fba04e93ce99f8d2d75370ef092df)

--- Quake/world.c
+++ Quake/world.c
@@ -395,13 +395,13 @@ void SV_FindTouchedLeafs (edict_t *ent, mnode_t *node)
 	if (node->contents == CONTENTS_SOLID)
 		return;
 
+	if (ent->num_leafs == MAX_ENT_LEAFS)
+		return;
+
 // add an efrag if the node is a leaf
 
 	if ( node->contents < 0)
 	{
-		if (ent->num_leafs == MAX_ENT_LEAFS)
-			return;
-
 		leaf = (mleaf_t *)node;
 		leafnum = leaf - sv.worldmodel->leafs - 1;
 
