c0cbbcacb4281fa89384652224ffd357278134c7
Andrei Drexler <makro@rq3.com>
2024-03-22 20:25:27 +0100

    Optimize BoxOnPlaneSide
    This increases performance significantly on replicon
    https://quaketastic.com/files/single_player/maps/replicon.zip
    https://www.celephais.net/board/view_thread.php?id=62305&start=
    
    (cherry picked from commit 51e59c481a82dbcb9364601e9ad948eaa6ca407f)

--- Quake/mathlib.c
+++ Quake/mathlib.c
@@ -111,6 +111,7 @@ Returns 1, 2, or 1 + 2
 int BoxOnPlaneSide (vec3_t emins, vec3_t emaxs, mplane_t *p)
 {
 	float	dist1, dist2;
+	int		xneg, yneg, zneg;
 	int		sides;
 
 #if 0	// this is done by the BOX_ON_PLANE_SIDE macro before calling this
@@ -126,46 +127,19 @@ int BoxOnPlaneSide (vec3_t emins, vec3_t emaxs, mplane_t *p)
 	}
 #endif
 
-// general case
-	switch (p->signbits)
-	{
-	case 0:
-		dist1 = p->normal[0]*emaxs[0] + p->normal[1]*emaxs[1] + p->normal[2]*emaxs[2];
-		dist2 = p->normal[0]*emins[0] + p->normal[1]*emins[1] + p->normal[2]*emins[2];
-		break;
-	case 1:
-		dist1 = p->normal[0]*emins[0] + p->normal[1]*emaxs[1] + p->normal[2]*emaxs[2];
-		dist2 = p->normal[0]*emaxs[0] + p->normal[1]*emins[1] + p->normal[2]*emins[2];
-		break;
-	case 2:
-		dist1 = p->normal[0]*emaxs[0] + p->normal[1]*emins[1] + p->normal[2]*emaxs[2];
-		dist2 = p->normal[0]*emins[0] + p->normal[1]*emaxs[1] + p->normal[2]*emins[2];
-		break;
-	case 3:
-		dist1 = p->normal[0]*emins[0] + p->normal[1]*emins[1] + p->normal[2]*emaxs[2];
-		dist2 = p->normal[0]*emaxs[0] + p->normal[1]*emaxs[1] + p->normal[2]*emins[2];
-		break;
-	case 4:
-		dist1 = p->normal[0]*emaxs[0] + p->normal[1]*emaxs[1] + p->normal[2]*emins[2];
-		dist2 = p->normal[0]*emins[0] + p->normal[1]*emins[1] + p->normal[2]*emaxs[2];
-		break;
-	case 5:
-		dist1 = p->normal[0]*emins[0] + p->normal[1]*emaxs[1] + p->normal[2]*emins[2];
-		dist2 = p->normal[0]*emaxs[0] + p->normal[1]*emins[1] + p->normal[2]*emaxs[2];
-		break;
-	case 6:
-		dist1 = p->normal[0]*emaxs[0] + p->normal[1]*emins[1] + p->normal[2]*emins[2];
-		dist2 = p->normal[0]*emins[0] + p->normal[1]*emaxs[1] + p->normal[2]*emaxs[2];
-		break;
-	case 7:
-		dist1 = p->normal[0]*emins[0] + p->normal[1]*emins[1] + p->normal[2]*emins[2];
-		dist2 = p->normal[0]*emaxs[0] + p->normal[1]*emaxs[1] + p->normal[2]*emaxs[2];
-		break;
-	default:
-		dist1 = dist2 = 0;		// shut up compiler
+	xneg = p->signbits & 1;
+	yneg = (p->signbits >> 1) & 1;
+	zneg = (p->signbits >> 2) & 1;
+
+	dist1 = p->normal[0] * (xneg ? emins : emaxs)[0] +
+			p->normal[1] * (yneg ? emins : emaxs)[1] +
+			p->normal[2] * (zneg ? emins : emaxs)[2];
+	dist2 = p->normal[0] * (xneg ? emaxs : emins)[0] +
+			p->normal[1] * (yneg ? emaxs : emins)[1] +
+			p->normal[2] * (zneg ? emaxs : emins)[2];
+
+	if (p->signbits & ~7)
 		Sys_Error ("BoxOnPlaneSide:  Bad signbits");
-		break;
-	}
 
 #if 0
 	int		i;
