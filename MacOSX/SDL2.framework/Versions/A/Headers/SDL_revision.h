0798fa95a7c77bc02ad091d31987ef344aae875c
Ozkan Sezer <sezeroz@gmail.com>
2025-02-26 22:56:02 +0300

    SDL rebuild for macOS

--- MacOSX/SDL2.framework/Versions/A/Headers/SDL_revision.h
+++ MacOSX/SDL2.framework/Versions/A/Headers/SDL_revision.h
@@ -1,2 +1,2 @@
-#define SDL_REVISION "https://github.com/sezero/SDL.git@b5fcddf2a00e8fafa25c66db19ce47b23ff11ba7"
+#define SDL_REVISION "https://github.com/sezero/SDL.git@c49e57b667aeb587e0b7a21750e465d7abd88db5"
 #define SDL_REVISION_NUMBER 0
